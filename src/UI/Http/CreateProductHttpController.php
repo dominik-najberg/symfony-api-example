76496bbae969acaf97b50e582481a3d218dabb24
Dominik Najberg <dominik.najberg@gmail.com>
2021-05-13 19:34:41 +0200

    Exception Handler

--- src/UI/Http/CreateProductHttpController.php
+++ src/UI/Http/CreateProductHttpController.php
@@ -6,8 +6,6 @@ use App\Application\Command\CreateProduct;
 use App\UI\Http\Request\CreateProductRequest;
 use App\UI\Http\Response\CreateProductResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 class CreateProductHttpController extends AbstractController
@@ -19,28 +17,18 @@ class CreateProductHttpController extends AbstractController
         $this->commandBus = $commandBus;
     }
 
-    public function __invoke(CreateProductRequest $createProductRequest): JsonResponse
+    public function __invoke(CreateProductRequest $createProductRequest): CreateProductResponse
     {
-        try {
-            $createProduct = new CreateProduct(
-                $createProductRequest->id(),
-                $createProductRequest->categoryId(),
-                $createProductRequest->name(),
-                $createProductRequest->description(),
-                $createProductRequest->amount(),
-                $createProductRequest->currency()
-            );
+        $createProduct = new CreateProduct(
+            $createProductRequest->id(),
+            $createProductRequest->categoryId(),
+            $createProductRequest->name(),
+            $createProductRequest->description(),
+            $createProductRequest->amount(),
+            $createProductRequest->currency()
+        );
 
-            $this->commandBus->dispatch($createProduct);
-        } catch (\Throwable $e) {
-            return new JsonResponse(
-                [
-                    'error' => true,
-                    'message' => $e->getMessage(),
-                ],
-                Response::HTTP_BAD_REQUEST
-            );
-        }
+        $this->commandBus->dispatch($createProduct);
 
         return CreateProductResponse::fromCommand($createProduct);
     }
